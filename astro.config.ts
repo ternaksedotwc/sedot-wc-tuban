72a8ef1335931fbcf6ab0bc451b2540c538af9d3
blog-andi <kawankawanbaru@gmail.com>
2026-02-14 09:23:35 +0800

    fix site

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://sedot-wc-tuban.pages.dev',
 
   integrations: [
     tailwind({
